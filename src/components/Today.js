05d10fd22c9bb3da17ba0a4875680e05424af99a
Jimmy Zeng <jzengg@gmail.com>
2018-10-31 18:14:50 -0400

    fix typo

--- src/components/Today.js
+++ src/components/Today.js
@@ -42,7 +42,7 @@ class Today extends Component {
 
   render() {
     const userId = localStorage.getItem(GC_USER_ID)
-    const dayStart = moment().startOf('day  ')
+    const dayStart = moment().startOf('day')
     const dayEnd = moment().endOf('day')
 
     const variables = {
