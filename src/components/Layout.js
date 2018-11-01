95993ca39b0bead741995dcb8b292861947531ab
Jimmy Zeng <jzengg@gmail.com>
2018-11-01 13:25:56 -0400

    fix layout on mobile

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,15 +7,18 @@ import styled from 'react-emotion'
 const Main = styled.main `
   max-width: 1200px;
   margin: 0 auto;
-  padding: 2rem;
+  padding: 1.2rem;
 `
 
 const Section = styled.section`
   display: flex;
+  justify-content: center;
 `
 
 const Column = styled.article`
   flex: 1;
+  box-sizing: border-box;
+  width: 100%;
 `
 
 class Layout extends Component {
