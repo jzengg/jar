7521745b4a5a925d6025e8050bba5c0bbd180a57
Jimmy Zeng <jzengg@gmail.com>
2018-11-05 09:46:53 -0500

    add spacing to history nav

--- src/components/HistoryNav.js
+++ src/components/HistoryNav.js
@@ -2,18 +2,24 @@ import React from 'react'
 
 import { SecondaryButton } from '../css/BaseButton'
 
+
+
 class HistoryNav extends React.Component {
   render () {
     const interval = this.props.interval
     const intervalOptions = this.props.intervalOptions
 
     return (
-        <nav>
+        <nav css={`
+            padding: 1.5rem;
+            `}>
           <SecondaryButton onClick={this.props.setPrevInterval}>
             back
           </SecondaryButton>
 
-          <select value={ interval } onChange={ this.props.updateInterval }>
+          <select css={`
+              margin: 0 1rem;
+              `} value={ interval } onChange={ this.props.updateInterval }>
             {intervalOptions.map(({ value, name }) => {
               return (
                 <option key={ value } value={ value }>
